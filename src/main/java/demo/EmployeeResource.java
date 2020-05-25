527c7afbb844b46c14b4fffbd997c1a4a1d1e161
nhan <an.nguyen@axonactive.com>
2020-05-25 17:20:29 +0700

    say hello

---
+++ src/main/java/demo/EmployeeResource.java
@@ -0,0 +1,25 @@
+package demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
+public class EmployeeResource {
+
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Map<String, String> get() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("Message", "Hello Azure!");
+		return map;
+	}
+
+}
